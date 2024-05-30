e7bd55104d9c5aa5f57193387469403a6e69789b
B2000144 <trib2000144@student.ctu.edu.vn>
2024-05-30 10:28:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/admin/register">register</router-link>
+    <router-link to="/admin/login">login</router-link>
   </nav>
   <router-view />
 </template>
