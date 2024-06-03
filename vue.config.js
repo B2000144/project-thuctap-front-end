a0068fe2ad79c580f482614e0873174eec77c16a
B2000144 <trib2000144@student.ctu.edu.vn>
2024-06-03 11:12:58 +0700

    cấu hình server Vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
